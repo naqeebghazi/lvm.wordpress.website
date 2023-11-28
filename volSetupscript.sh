9ae8a9dedce345372adf6a053b46d67afd84be64
naqeebghazi <naqeebghazi@outlook.com>
2023-11-28 22:35:31 +0000

    images

--- volSetupscript.sh
+++ volSetupscript.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
+# /etc/fstab
+#
+#
+#
+#
+#
+# mounts for wordpress webserver
+UUID=0c34dba8-4698-46f5-8ac6-b290574fe73b       /var/www/html       ext4     defaults        0       0
+UUID=d4dc2e72-a8c0-4439-abd1-fd941ca853dd       /var/log            ext4     defaults        0       0
 
-echo "Show all block storage devices"
-lsblk
 
-
-sudo gdisk /dev/xvdb
-sudo gdisk /dev/xvdc
-sudo gdisk /dev/xvdd
-
+# mounts for db server
+UUID=86c93a31-63a5-49ed-ae25-e7444833f735        /db                ext4     defaults        0       0
+UUID=1b076527-6214-4e7e-8068-7335ee8d9672       /var/log            ext4     defaults        0       0
